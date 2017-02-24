7334545196db59d810635c6bb02d5d02870993b5
Kendra Lyndon <kendralyndon@gmail.com>
2017-02-24 12:47:56 -0700

    first commit clean project

---
+++ public/app.js
@@ -0,0 +1,26 @@
+var attlApp = angular.module('attlApp', ['ngRoute', 'ngMap']);
+
+attlApp.config(function($routeProvider, $locationProvider) {
+
+  $routeProvider
+    .when('/', {
+      templateUrl: 'partials/home.html',
+      controller: 'HomeController'
+    })
+    .when('/about', {
+      templateUrl: 'partials/about.html',
+      controller: 'AboutController'
+    })
+    .when('/news', {
+      templateUrl: 'partials/news.html',
+      controller: 'NewsController'
+    })
+    .when('/contact', {
+      templateUrl: 'partials/contact.html'
+    })
+    .otherwise('/', {
+      redirectTo : '/'
+    })
+
+  $locationProvider.html5Mode(true);
+});
